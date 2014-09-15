1bba802177231010e00747014888077df414d9ca
will123195 <will123195@gmail.com>
2014-09-15 15:21:29 -0400

    serve condor

---
+++ buildTracker.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var fs    = require('fs')
+var getIP = require('external-ip')()
+var bs    = require('browserify-string')
+var path  = require('path')
+
+var script = fs.readFileSync(path.join(__dirname, 'condor.template'), {
+  encoding: 'utf8'
+})
+
+var condor = module.exports = {}
+
+getIP(function (err, ip) {
+  if (err) {
+    console.log(err)
+    ip = "' + location.hostname + '"
+    return
+  }
+
+  script = script.replace(/{{HOST}}/g, ip)
+
+  browserify(script)
+
+});
+
+function browserify(moduleString) {
+  bs(moduleString).bundle(function (err, src) {
+    if (err) {
+      console.log(err)
+      condor.script = ''
+      return
+    }
+    condor.script = src
+  });
+}
