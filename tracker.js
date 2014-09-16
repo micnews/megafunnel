d957a55a613e0b529fb916f20871b4c4410284c0
Dominic Tarr <dominic.tarr@gmail.com>
2014-09-16 18:53:35 +0300

    update static tracker.js

--- tracker.js
+++ tracker.js
@@ -2,7 +2,12 @@
 
 var url = require('url')
 
-var obj = url.parse(document.currentScript.src)
+//ie8 polyfil
+require('document.currentscript')
+
+var src = (document.currentScript || document._currentScript).src
+
+var obj = url.parse(src)
 
 delete obj.pathname
 delete obj.href
@@ -15,7 +20,7 @@ require('condor/xhr')({
   uri: url.format(obj)
 })
 
-},{"condor/xhr":19,"url":7}],2:[function(require,module,exports){
+},{"condor/xhr":19,"document.currentscript":21,"url":7}],2:[function(require,module,exports){
 // shim for using process in browser
 
 var process = module.exports = {};
@@ -2068,7 +2073,7 @@ module.exports = function (opts) {
 
 }
 
-},{"./condor":8,"debounce":14,"xhr":21}],20:[function(require,module,exports){
+},{"./condor":8,"debounce":14,"xhr":22}],20:[function(require,module,exports){
 var map = function (input, fn) {
       var result = Array(input.length)
 
@@ -2126,6 +2131,202 @@ function createCSV (options, CSV) {
 module.exports = createCSV({escapeNewlines: true}, createCSV)
 
 },{}],21:[function(require,module,exports){
+/*!
+ * document.currentScript
+ * Polyfill for `document.currentScript`.
+ * Copyright (c) 2014 James M. Greene
+ * Licensed MIT
+ * http://jsfiddle.net/JamesMGreene/9DFc9/
+ * v0.1.5
+ */
+(function() {
+
+
+var hasStackBeforeThrowing = false,
+    hasStackAfterThrowing = false;
+(function() {
+  try {
+    var err = new Error();
+    hasStackBeforeThrowing = typeof err.stack === "string" && !!err.stack;
+    throw err;
+  }
+  catch (thrownErr) {
+    hasStackAfterThrowing = typeof thrownErr.stack === "string" && !!thrownErr.stack;
+  }
+})();
+
+
+// This page's URL
+var pageUrl = window.location.href;
+
+// Live NodeList collection
+var scripts = document.getElementsByTagName("script");
+
+// Get script object based on the `src` URL
+function getScriptFromUrl(url) {
+  if (typeof url === "string" && url) {
+    for (var i = 0, len = scripts.length; i < len; i++) {
+      if (scripts[i].src === url) {
+        return scripts[i];
+      }
+    }
+  }
+  return null;
+}
+
+// If there is only a single inline script on the page, return it; otherwise `null`
+function getSoleInlineScript() {
+  var script = null;
+  for (var i = 0, len = scripts.length; i < len; i++) {
+    if (!scripts[i].src) {
+      if (script) {
+        return null;
+      }
+      script = scripts[i];
+    }
+  }
+  return script;
+}
+
+// Get the configured default value for how many layers of stack depth to ignore
+function getStackDepthToSkip() {
+  var depth = 0;
+  if (
+    typeof _currentScript !== "undefined" &&
+    _currentScript &&
+    typeof _currentScript.skipStackDepth === "number"
+  ) {
+    depth = _currentScript.skipStackDepth;
+  }
+  return depth;
+}
+
+// Get the currently executing script URL from an Error stack trace
+function getScriptUrlFromStack(stack, skipStackDepth) {
+  var url, matches, remainingStack,
+      ignoreMessage = typeof skipStackDepth === "number";
+  skipStackDepth = ignoreMessage ? skipStackDepth : getStackDepthToSkip();
+  if (typeof stack === "string" && stack) {
+    if (ignoreMessage) {
+      matches = stack.match(/(data:text\/javascript(?:;[^,]+)?,.+?|(?:|blob:)(?:http[s]?|file):\/\/[\/]?.+?\/[^:\)]*?)(?::\d+)(?::\d+)?/);
+    }
+    else {
+      matches = stack.match(/^(?:|[^:@]*@|.+\)@(?=data:text\/javascript|blob|http[s]?|file)|.+?\s+(?: at |@)(?:[^:\(]+ )*[\(]?)(data:text\/javascript(?:;[^,]+)?,.+?|(?:|blob:)(?:http[s]?|file):\/\/[\/]?.+?\/[^:\)]*?)(?::\d+)(?::\d+)?/);
+
+      if (!(matches && matches[1])) {
+        matches = stack.match(/\)@(data:text\/javascript(?:;[^,]+)?,.+?|(?:|blob:)(?:http[s]?|file):\/\/[\/]?.+?\/[^:\)]*?)(?::\d+)(?::\d+)?/);
+      }
+    }
+
+    if (matches && matches[1]) {
+      if (skipStackDepth > 0) {
+        remainingStack = stack.slice(stack.indexOf(matches[0]) + matches[0].length);
+        url = getScriptUrlFromStack(remainingStack, (skipStackDepth - 1));
+      }
+      else {
+        url = matches[1];
+      }
+    }
+  }
+  return url;
+}
+
+// Get the currently executing `script` DOM element
+function _currentScript() {
+  // Yes, this IS actually possible
+  if (scripts.length === 0) {
+    return null;
+  }
+
+  if (scripts.length === 1) {
+    return scripts[0];
+  }
+
+  if ("readyState" in scripts[0]) {
+    for (var i = scripts.length; i--; ) {
+      if (scripts[i].readyState === "interactive") {
+        return scripts[i];
+      }
+    }
+  }
+
+  if (document.readyState === "loading") {
+    return scripts[scripts.length - 1];
+  }
+
+  var stack,
+      e = new Error();
+  if (hasStackBeforeThrowing) {
+    stack = e.stack;
+  }
+  if (!stack && hasStackAfterThrowing) {
+    try {
+      throw e;
+    }
+    catch (err) {
+      // NOTE: Cannot use `err.sourceURL` or `err.fileName` as they will always be THIS script
+      stack = err.stack;
+    }
+  }
+  if (stack) {
+    var url = getScriptUrlFromStack(stack);
+    var script = getScriptFromUrl(url);
+    if (!script && url === pageUrl) {
+      script = getSoleInlineScript();
+    }
+    return script;
+  }
+
+  return null;
+}
+
+
+// Configuration
+_currentScript.skipStackDepth = 1;
+
+
+
+// Inspect the polyfill-ability of this browser
+var needsPolyfill = !("currentScript" in document);
+var canDefineGetter = document.__defineGetter__;
+var canDefineProp = typeof Object.defineProperty === "function" &&
+  (function() {
+    var result;
+    try {
+      Object.defineProperty(document, "_xyz", {
+        get: function() {
+          return "blah";
+        },
+        configurable: true
+      });
+      result = document._xyz === "blah";
+      delete document._xyz;
+    }
+    catch (e) {
+      result = false;
+    }
+    return result;
+  })();
+
+
+// Add the "private" property for testing, even if the real property can be polyfilled
+document._currentScript = _currentScript;
+
+// Polyfill it!
+if (needsPolyfill) {
+  if (canDefineProp) {
+    Object.defineProperty(document, "currentScript", {
+      get: _currentScript
+    });
+  }
+  else if (canDefineGetter) {
+    document.__defineGetter__("currentScript", _currentScript);
+  }
+}
+
+})();
+
+},{}],22:[function(require,module,exports){
 var window = require("global/window")
 var once = require("once")
 var parseHeaders = require('parse-headers')
@@ -2297,7 +2498,7 @@ function createXHR(options, callback) {
 
 function noop() {}
 
-},{"global/window":22,"once":23,"parse-headers":27}],22:[function(require,module,exports){
+},{"global/window":23,"once":24,"parse-headers":28}],23:[function(require,module,exports){
 (function (global){
 if (typeof window !== "undefined") {
     module.exports = window
@@ -2308,7 +2509,7 @@ if (typeof window !== "undefined") {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],23:[function(require,module,exports){
+},{}],24:[function(require,module,exports){
 module.exports = once
 
 once.proto = once(function () {
@@ -2329,13 +2530,13 @@ function once (fn) {
   }
 }
 
-},{}],24:[function(require,module,exports){
+},{}],25:[function(require,module,exports){
 module.exports=require(15)
-},{"is-function":25}],25:[function(require,module,exports){
+},{"is-function":26}],26:[function(require,module,exports){
 module.exports=require(16)
-},{}],26:[function(require,module,exports){
-module.exports=require(12)
 },{}],27:[function(require,module,exports){
+module.exports=require(12)
+},{}],28:[function(require,module,exports){
 var trim = require('trim')
   , forEach = require('for-each')
 
@@ -2357,4 +2558,4 @@ module.exports = function (headers) {
 
   return result
 }
-},{"for-each":24,"trim":26}]},{},[1]);
+},{"for-each":25,"trim":27}]},{},[1]);
